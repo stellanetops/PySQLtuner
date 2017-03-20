fc4ee63c55fa2c9bbb35f03eba9d012eb91b3893
immanuelqrw <immanuelqrw@gmail.com>
2017-03-20 19:16:07 -0400

    moved startup info from init to script

--- scripts/tuner.py
+++ scripts/tuner.py
@@ -2,3 +2,14 @@
 Reviews a MySQL installation
 Allows tuning to increase performance and stability
 """
+
+import argparse
+import platform
+import pysqltuner as tuner
+import pysqltuner.fancy_print as fp
+
+if __name__ == "__main__":
+    option: tuner.Option = tuner.Option()
+    os_name: str = platform.system()
+    if os_name == "MSWin32":
+        fp.info_print(f"* Windows OS({os_name}) is not fully supported", option)
